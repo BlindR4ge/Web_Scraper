f8617938a5230dc4b3d984101640f78652f92f86
BlindR4ge <dk2004@bk.ru>
2023-12-13 16:28:42 +0500

    Changed DB from SQLite to SQLAlchemy

--- Scraper/web_scraper.py
+++ Scraper/web_scraper.py
@@ -1,47 +1,43 @@
 from bs4 import BeautifulSoup as bs
-import sqlite3 as sl
-import pandas as pd
 import requests
+from Web_Scraper import config
+from Database import Market_Bin
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
-class SQLiteConnection:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.connection = None
+market_list = []
 
-    def __enter__(self):
-        self.connection = sl.connect(self.db_file)
-        return self.connection
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.connection:
-            self.connection.close()
-
-
-def df_filling(item_group, cat):
+def df_filling(item_group, cat, market_list):
     for items in item_group:
         name = items.find('div', class_='link-wrap').text
         old_price = int(items.find('mark', class_='old-price').text[:-3].replace(' ', ''))
         new_price = int(items.find('mark', class_='price').text[:-3].replace(' ', ''))
         discount = int(100 - round(new_price / old_price * 100, 0))
         item_info = [name.strip(), old_price, new_price, cat, discount]
-        length = len(df)
-        df.loc[length] = item_info
+        market_list.append(item_info)
 
 
-url = 'https://airsoft-rus.ru/'
-req = requests.get(url)
-bsObject = bs(req.text, "html.parser")
+engine = create_engine(config.DATABASE_CONNECTION_URI)
+engine.connect()
+session = Session(engine)
 
+while(True):
+    url = 'https://airsoft-rus.ru/'
+    req = requests.get(url)
+    bsObject = bs(req.text, "lxml")
 
-data = bsObject.find_all('section', class_='popular-section')
-sales = data[0].find_all('div', class_='item')
-hits = data[2].find_all('div', class_='item')
+    data = bsObject.find_all('section', class_='popular-section')
+    sales = data[0].find_all('div', class_='item')
+    hits = data[2].find_all('div', class_='item')
 
-df = pd.DataFrame(columns=['Товар', 'Старая_цена', 'Новая_цена', 'Категория', 'Скидка'])
+    df_filling(sales, "Распродажа", market_list)
+    df_filling(hits, "Хит продаж", market_list)
 
-df_filling(sales, "Распродажа")
-df_filling(hits, "Хит продаж")
-
-with SQLiteConnection('../items.db') as con:
-    df.to_sql(name='items', con=con, if_exists='append', index=False)
+    for item in market_list:
+        tovar = Market_Bin(item=item[0], old_price=item[1], new_price=item[2], category=item[3], discount=item[4])
+        result = session.query(Market_Bin)
+        print(result)
+        if (result == None):
+            session.add(Market_Bin)
+            session.commit()
 
