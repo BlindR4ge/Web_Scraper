729cc01debc53e9ad4164adbaa11b681ef9d987a
BlindR4ge <dk2004@bk.ru>
2023-12-13 15:12:35 +0500

    Fixed SQL-Injection waekness

--- App/app.py
+++ App/app.py
@@ -27,11 +27,12 @@ def home():
 @app.route('/result', methods=["get"])
 def result():
     cat = request.args.get('hidden')
-    with SQLiteConnection('../items.db') as con:
-        cat_min_price = con.execute("SELECT MIN(Новая_цена) FROM items WHERE Категория = ?", (cat,)).fetchone()
-        output = con.execute("SELECT Товар, Новая_цена, Скидка FROM items WHERE Категория = ? AND Новая_цена = ?",
-                             (cat, cat_min_price[0])).fetchone()
-        print(f'''Товар: {output[0]}\nЦена с учетом скидки : {output[1]}р.\nСкидка: {output[2]}%''')
+    if cat.isalnum():
+        with SQLiteConnection('../items.db') as con:
+            cat_min_price = con.execute("SELECT MIN(Новая_цена) FROM items WHERE Категория = ?", (cat,)).fetchone()
+            output = con.execute("SELECT Товар, Новая_цена, Скидка FROM items WHERE Категория = ? AND Новая_цена = ?",
+                                 (cat, cat_min_price[0])).fetchone()
+    print(f'''Товар: {output[0]}\nЦена с учетом скидки : {output[1]}р.\nСкидка: {output[2]}%''')
     return rt("resultpage.html", item=output[0], price=output[1], discount=output[2])
 
 
