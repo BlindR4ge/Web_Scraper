8a4c36f9969a677b7f4d22f1486bd3fb65936e46
BlindR4ge <dk2004@bk.ru>
2023-12-15 16:15:44 +0500

    moved query-specifier to separate file

--- App/app.py
+++ App/app.py
@@ -1,12 +1,11 @@
 from flask import render_template as rt, request
 import App_generator
 from db_config import MarketBin
+import queryMaker
 
 app = App_generator.create_app()
 
-def get(model):
-    data = model.query.get()
-    return data
+
 
 @app.route('/', methods=["get", "post"])
 def home():
@@ -16,12 +15,12 @@ def home():
 @app.route('/result', methods=["get"])
 def result():
     cat = request.args.get('hidden')
-    item = get(MarketBin).filter(category=cat, new_price=min(MarketBin.new_price))
+    item = queryMaker.get(MarketBin).filter(category=cat, new_price=min(MarketBin.new_price))
     #cat_min_price = con.execute("SELECT MIN(Новая_цена) FROM items WHERE Категория = ?", (cat,)).fetchone()
     #output = con.execute("SELECT Товар, Новая_цена, Скидка FROM items WHERE Категория = ? AND Новая_цена = ?",
                                  #(cat, cat_min_price[0])).fetchone()
-    return rt("resultpage.html", item)
+    return rt("resultpage.html", item=item[1], price=item[3], discount=item[5])
 
 
 if __name__ == '__main__':
-    app.run(host='128.0.0.0', port=30010, debug=True)
+    app.run(host='128.0.0.1', port=30010, debug=True)
